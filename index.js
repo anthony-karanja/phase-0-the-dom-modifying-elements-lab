22c0ef91227ffaabaff895b0849f8bb9e0f97fd8
Anthony <wangonguanthony@gmail.com>
2025-03-23 22:30:48 +0300

    The Dom modifying elements

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+// Remove main
+document.querySelector("main").remove();
+
+let newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+newHeader.textContent = "LARRY KIPKURUI  is the champion";
+
+// Append the new
+document.body.append(newHeader);
